7ae73965f27530f42dfca19bc91151bb48d88e66
Taichi Uragami <backpaper0@gmail.com>
2015-02-24 22:42:57 +0900

    Initial commit

---
+++ desktop/src/sample/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package sample.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import sample.KotlinSample;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KotlinSample(), config);
+	}
+}
